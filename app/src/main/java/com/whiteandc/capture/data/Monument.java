f587dcbf5af563c5c487ef1b7a039cbd3a47b9fe
Blanca Gomez Andrades <blanca_piper@hotmail.com>
2015-05-11 02:47:15 +0200

    Anadido mapas y monumentos de Madrid

--- app/src/main/java/com/whiteandc/capture/data/Monument.java
+++ app/src/main/java/com/whiteandc/capture/data/Monument.java
@@ -3,6 +3,8 @@ package com.whiteandc.capture.data;
 
 import android.graphics.drawable.Drawable;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class Monument implements Comparable{
 	
 	private String name= null;
@@ -11,13 +13,23 @@ public class Monument implements Comparable{
 	private String capturedImg= null;
 	private String description= "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 
+    public LatLng getLatLng() {
+        return latLng;
+    }
+
+    public void setLatLng(LatLng latLng) {
+        this.latLng = latLng;
+    }
+
+    private LatLng latLng= null;
     private Drawable mainPicture = null;
 
-	public Monument(String name, int[] photoList, boolean captured, String capturedImage){
+	public Monument(String name, int[] photoList, boolean captured, String capturedImage, LatLng latLng){
 		this.name= name;
 		photos= photoList;
 		this.captured= captured;
 		capturedImg= capturedImage;
+        this.latLng= latLng;
 	}
 	public String getName() {
 		return name;
